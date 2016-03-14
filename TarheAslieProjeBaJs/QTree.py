2dd07414bb8454abda58bc4c734098f92c8f4392
Your Name <you@example.com>
2016-03-14 15:34:45 +0330

    Original Program With JavaScript Addietion Has Been Added

---
+++ TarheAslieProjeBaJs/QTree.py
@@ -0,0 +1,53 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+import os
+
+dir_list = []
+
+class Main(QtGui.QTreeView):
+
+    def __init__(self,parent=None):
+        QtGui.QTreeView.__init__(self)
+        self.treeview = QtGui.QTreeView(self)
+        self.treeview.resize(730, 280)
+        self.treeview.model = QtGui.QFileSystemModel()
+        self.treeview.model.setRootPath( QtCore.QDir.currentPath() )
+        self.treeview.setModel(self.treeview.model)
+        self.treeview.setColumnWidth(0, 200)
+        self.treeview.clicked.connect(self.get_file_path)
+        self.treeview.doubleClicked.connect(self.test)
+        self.filePath = None
+
+
+
+    def test(self, index):
+        indexItem = self.treeview.model.index(index.row(), 0, index.parent())
+        self.fileName = self.treeview.model.fileName(indexItem)
+        self.filePath = self.treeview.model.filePath(indexItem)
+        if (os.path.isfile(self.filePath)):
+            path = str('"' + self.filePath + '"')
+            os.system(str(path))
+        else:
+            pass
+
+    def get_file_path(self, index):
+        indexItem = self.treeview.model.index(index.row(), 0, index.parent())
+        self.fileName = self.treeview.model.fileName(indexItem)
+        self.filePath = self.treeview.model.filePath(indexItem)
+
+
+    def getFilePath(self):
+        return self.filePath
+    def getFileName(self):
+        return self.fileName
+
+
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    w = Main()
+    w.show()
+    sys.exit(app.exec_())
